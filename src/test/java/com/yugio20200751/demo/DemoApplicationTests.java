4398f5512a59e71672bef61c58fbe43e97508d02
parkdsdsds <reddevilmu2000@gmail.com>
2025-06-11 20:47:38 +0900

    Gradle wrapper 설정 및 기본 테스트 클래스 추가

---
+++ src/test/java/com/yugio20200751/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yugio20200751.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
